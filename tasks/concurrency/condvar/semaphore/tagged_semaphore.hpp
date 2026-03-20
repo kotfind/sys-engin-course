c1517c63f03c98fd16062d6eac11d71071c5dc93
kotfind <kotfind@yandex.ru>
2026-03-20 17:51:51 +0300

    solve

--- tasks/concurrency/condvar/semaphore/tagged_semaphore.hpp
+++ tasks/concurrency/condvar/semaphore/tagged_semaphore.hpp
@@ -8,75 +8,75 @@ namespace solutions {
 
 template <typename Tag>
 class TaggedSemaphore {
- public:
-  // No affine types in C++ =(
-  class Token {
-    friend class TaggedSemaphore;
+  public:
+    // No affine types in C++ =(
+    class Token {
+        friend class TaggedSemaphore;
 
-   public:
-    ~Token() {
-      assert(!valid_);
+      public:
+        ~Token() {
+            assert(!valid_);
+        }
+
+        // Non-copyable
+        Token(const Token&) = delete;
+        Token& operator=(const Token&) = delete;
+
+        // Movable
+
+        Token(Token&& that) {
+            that.Invalidate();
+        }
+
+        Token& operator=(Token&& that) = delete;
+
+      private:
+        Token() = default;
+
+        void Invalidate() {
+            assert(valid_);
+            valid_ = false;
+        }
+
+      private:
+        bool valid_{true};
+    };
+
+    class Guard {
+      public:
+        explicit Guard(TaggedSemaphore& host)
+            : host_(host), token_(host_.Acquire()) {
+        }
+
+        ~Guard() {
+            host_.Release(std::move(token_));
+        }
+
+      private:
+        TaggedSemaphore& host_;
+        Token token_;
+    };
+
+  public:
+    explicit TaggedSemaphore(size_t tokens) : impl_(tokens) {
     }
 
-    // Non-copyable
-    Token(const Token&) = delete;
-    Token& operator=(const Token&) = delete;
-
-    // Movable
-
-    Token(Token&& that) {
-      that.Invalidate();
+    Token Acquire() {
+        impl_.Acquire();
+        return Token{};
     }
 
-    Token& operator=(Token&& that) = delete;
-
-   private:
-    Token() = default;
-
-    void Invalidate() {
-      assert(valid_);
-      valid_ = false;
+    void Release(Token&& token) {
+        impl_.Release();
+        token.Invalidate();
     }
 
-   private:
-    bool valid_{true};
-  };
-
-  class Guard {
-   public:
-    explicit Guard(TaggedSemaphore& host)
-        : host_(host), token_(host_.Acquire()) {
+    Guard MakeGuard() {
+        return Guard{*this};
     }
 
-    ~Guard() {
-      host_.Release(std::move(token_));
-    }
-
-   private:
-    TaggedSemaphore& host_;
-    Token token_;
-  };
-
- public:
-  explicit TaggedSemaphore(size_t tokens) : impl_(tokens) {
-  }
-
-  Token Acquire() {
-    impl_.Acquire();
-    return Token{};
-  }
-
-  void Release(Token&& token) {
-    impl_.Release();
-    token.Invalidate();
-  }
-
-  Guard MakeGuard() {
-    return Guard{*this};
-  }
-
- private:
-  Semaphore impl_;
+  private:
+    Semaphore impl_;
 };
 
-}  // namespace solutions
+} // namespace solutions

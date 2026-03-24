508c4ffaac7dad7bb46783cfb71534d920b81d1c
kotfind <kotfind@yandex.ru>
2026-03-24 21:59:00 +0300

    move `run` to separate file

--- tasks/filesystem/fuse/main.cpp
+++ tasks/filesystem/fuse/main.cpp
@@ -1,42 +1,6 @@
 #include "args.hpp"
-#include "fuse_cpu.hpp"
-#include "log.hpp"
-#include "signal_handler.hpp"
-
-#include <condition_variable>
-#include <memory>
-#include <mutex>
-
-using namespace std::chrono_literals;
-
-int run(const Args& args) {
-    bool is_running = true;
-    std::condition_variable is_running_cv;
-
-    if (!install_signal_handler([&](int signal) {
-            is_running = false;
-            is_running_cv.notify_all();
-            info("Got signal={}. Exiting...", signal);
-        })) {
-        return 1;
-    }
-
-    auto cpu = std::unique_ptr<FuseCpu>(
-        FuseCpu::create(args.mount_point, args.unit_count)
-    );
-    if (cpu == nullptr) {
-        return 1;
-    }
-
-    std::mutex mutex;
-    std::unique_lock lock{mutex};
-    is_running_cv.wait(lock, [&]() { return !is_running; });
-
-    return 0;
-}
+#include "run.hpp"
 
 int main(int argc, char** argv) {
-    auto args = parse_args(argc, argv);
-
-    return run(args);
+    return run(parse_args(argc, argv));
 }
---
+++ tasks/filesystem/fuse/run.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+struct Args;
+
+int run(const Args& args);

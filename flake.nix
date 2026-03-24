c83b32befa8b5830d51f97b07e1be36e58652e33
kotfind <kotfind@yandex.ru>
2026-03-25 00:15:44 +0300

    build with nix

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
       pkgs = import nixpkgs {inherit system;};
 
       inherit (pkgs) mkShell;
+      inherit (pkgs.stdenv) mkDerivation;
 
       shell = mkShell {
         name = "sys-engin-course-shell";
@@ -37,7 +38,34 @@
 
         hardeningDisable = ["all"];
       };
+
+      app = mkDerivation {
+        pname = "fuse-cpu";
+        version = "0.1.0";
+
+        src = ./.;
+
+        nativeBuildInputs = with pkgs; [
+          catch2
+          clang
+          cmake
+          mold
+          fuse3
+          fuse3.dev
+          doxygen
+          pkg-config
+        ];
+
+        buildInputs = with pkgs; [
+          fuse3
+        ];
+      };
     in {
       devShells.default = shell;
+
+      packages = {
+        default = app;
+        fuse-cpu = app;
+      };
     });
 }
